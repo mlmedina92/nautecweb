29515406942f7739a1a77417a249f27509832ead
mlmedina92 <lm30540@gmail.com>
2024-07-03 11:20:30 -0300

    validation captcitacion

--- src/Validations/validations.js
+++ src/Validations/validations.js
@@ -45,3 +45,38 @@ export function preferenceCon(preference) {
     }
     return errors
 }
+
+export function nameCap(name) {
+    if (name.length < 1) {
+        errors = { ...errors, name: 'Llena este campo obligatorio' }
+    } else {
+        errors = { ...errors, name: "" }
+    }
+    return errors
+}
+
+export function phoneCap(phone) {
+    if (phone.length < 1) {
+        errors = { ...errors, phone: 'Llena este campo obligatorio' }
+    } else if (!onlyNumbers.test(phone)) {
+        errors = { ...errors, phone: 'El teléfono debe contener solo números' }
+    } else if (phone.length > 13) {
+        errors = { ...errors, phone: 'Ingresa un número de teléfono válido' }
+    } else {
+        errors = { ...errors, phone: "" }
+    }
+    return errors
+}
+
+export function emailCap(email) {
+    if (email.length < 1) {
+        errors = { ...errors, email: 'Llena este campo obligatorio' }
+    } else if (!regexEmail.test(email)) {
+        errors = { ...errors, email: 'Favor de registrar una dirección de correo electrónico verdadera' }
+    } else if (email.length > 40) {
+        errors = { ...errors, email: 'El email no debe superar los 40 caracteres' }
+    } else {
+        errors = { ...errors, email: "" }
+    }
+    return errors
+}
